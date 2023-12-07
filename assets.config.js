c278fb823ffd9ab547f4f2b16e5d2475c806b001
Thomas Günther <mail@medienbaecker.com>
2023-12-07 17:30:47 +0100

    Initial commit

---
+++ assets.config.js
@@ -0,0 +1,92 @@
+// --------------------------------------------------
+// 🧰 Tools
+// --------------------------------------------------
+
+import esbuild from "esbuild";
+import browserSync from "browser-sync";
+
+// --------------------------------------------------
+// 🔧 Setup
+// --------------------------------------------------
+
+// Setting the valet domain by appending .test to the directory name
+const directoryPath = process.cwd(); 
+const directoryName = directoryPath.split("/").pop();
+const domain = `${directoryName}.test`;
+
+// JavaScript files
+const jsFiles = [
+  "assets/js/main.js"
+]
+
+// CSS files
+const cssFiles = [
+  "assets/css/style.css"
+]
+
+// --------------------------------------------------
+// ⚡ BrowserSync
+// --------------------------------------------------
+
+const browserSyncInstance = browserSync.create();
+
+browserSyncInstance.init({
+  proxy: domain, // Set the proxy to the valet domain
+  host: domain, // Set the host to the valet domain
+  open: "external", // Automatically open the valet domain in the browser
+  reloadOnRestart: true, // Reload the browser when we restart the server
+  notify: false, // I don't want to see the BrowserSync notification in the browser
+  ui: false, // Disable the BrowserSync UI
+});
+
+// --------------------------------------------------
+// ✨ JavaScript
+// Bundles and minifies JavaScript files
+// --------------------------------------------------
+
+const jsContext = await esbuild.context({
+  entryPoints: jsFiles,
+  outdir: "assets/js",
+  outExtension: {
+    ".js": ".min.js"
+  },
+  minify: true,
+  bundle: true,
+  sourcemap: true,
+})
+
+await jsContext.watch();
+
+// --------------------------------------------------
+// 🎨 CSS
+// Compiles and minifies CSS files
+// --------------------------------------------------
+
+const cssContext = await esbuild.context({
+  entryPoints: cssFiles,
+  outdir: "assets/css",
+  outExtension: {
+    ".css": ".min.css"
+  },
+  minify: true,
+  sourcemap: true,
+  target: ["chrome88", "firefox78", "safari14"],
+})
+
+await cssContext.watch();
+
+// --------------------------------------------------
+// 📁 Other files
+// --------------------------------------------------
+
+browserSyncInstance.watch([
+  "assets/css/*.css",
+  "assets/js/*.min.js",
+  "assets/images/**",
+  "site/*/**",
+  "!site/sessions/**",
+  "!site/cache/**",
+  "content/**/*.*"
+]).on("change", (file) => {
+  browserSyncInstance.reload(file);
+});
